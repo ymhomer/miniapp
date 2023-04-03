88ad8e7f7f817f4b3e3396fa5df368fe91ef28ab
ymhomer <ymhomer.4253@gmail.com>
2023-04-04 05:18:49 +0800

    JS file upload

---
+++ js/init-firebase.js
@@ -0,0 +1,14 @@
+// TODO: Replace the following with your app's Firebase project configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyC18bQBbI68A89aUmkdvUtBzOCkfeVeECk",
+  //authDomain: "project-id.firebaseapp.com",
+  //databaseURL: "https://project-id.firebaseio.com",
+  projectId: "taekwondozero",
+  //storageBucket: "project-id.appspot.com",
+  //messagingSenderId: "sender-id",
+  //appId: "app-id",
+  //measurementId: "G-measurement-id",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
