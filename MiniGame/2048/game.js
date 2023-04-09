7a2c6bc4459a9bafbe50dc3af9ab12633cc19c48
ymhomer <ymhomer.4253@gmail.com>
2023-04-09 14:39:08 +0800

    Update game.js

--- MiniGame/2048/game.js
+++ MiniGame/2048/game.js
@@ -87,90 +87,90 @@ var GameManager = {
   },
 
   // 移动方块
-move: function(direction) {
-  var moved = false;
-  var self = this;
-  var nextX, nextY, currentTileValue;
-  var startX, startY, deltaX, deltaY;
-  var canMerge;
+  move: function(direction) {
+    var moved = false;
+    var self = this;
+    var nextX, nextY, currentTileValue;
+    var startX, startY, deltaX, deltaY;
+    var canMerge;
 
-  switch (direction) {
+    switch (direction) {
     case 'left':
       for (var i = 0; i < this.size; i++) {
-        for (var j = 1; j < this.size; j++) {
-          if (this.board[i][j] !== 0) {
-            currentTileValue = this.board[i][j];
-            nextX = i;
-            nextY = j - 1;
-            canMerge = false;
-            while (nextY >= 0 && this.board[nextX][nextY] === 0) {
-              nextY--;
-            }
-            if (nextY >= 0 && this.board[nextX][nextY] === currentTileValue) {
-              canMerge = true;
-            }
-            if (j !== nextY + 1) {
-              moved = true;
-            }
-            this.board[i][j] = 0;
-            this.board[nextX][nextY + (canMerge ? 0 : 1)] = canMerge ? currentTileValue * 2 : currentTileValue;
-          }
+      for (var j = 1; j < this.size; j++) {
+        if (this.board[i][j] !== 0) {
+        currentTileValue = this.board[i][j];
+        nextX = i;
+        nextY = j - 1;
+        canMerge = false;
+        while (nextY >= 0 && this.board[nextX][nextY] === 0) {
+          nextY--;
         }
+        if (nextY >= 0 && this.board[nextX][nextY] === currentTileValue) {
+          canMerge = true;
+        }
+        if (j !== nextY + 1) {
+          moved = true;
+        }
+        this.board[i][j] = 0;
+        this.board[nextX][nextY + (canMerge ? 0 : 1)] = canMerge ? currentTileValue * 2 : currentTileValue;
+        }
+      }
       }
       break;
 
     case 'right':
       for (var i = 0; i < this.size; i++) {
-        for (var j = this.size - 2; j >= 0; j--) {
-          if (this.board[i][j] !== 0) {
-            currentTileValue = this.board[i][j];
-            nextX = i;
-            nextY = j + 1;
-            canMerge = false;
-            while (nextY < this.size && this.board[nextX][nextY] === 0) {
-              nextY++;
-            }
-            if (nextY < this.size && this.board[nextX][nextY] === currentTileValue) {
-              canMerge = true;
-            }
-            if (j !== nextY - 1) {
-              moved = true;
-            }
-            this.board[i][j] = 0;
-            this.board[nextX][nextY - (canMerge ? 0 : 1)] = canMerge ? currentTileValue * 2 : currentTileValue;
-          }
+      for (var j = this.size - 2; j >= 0; j--) {
+        if (this.board[i][j] !== 0) {
+        currentTileValue = this.board[i][j];
+        nextX = i;
+        nextY = j + 1;
+        canMerge = false;
+        while (nextY < this.size && this.board[nextX][nextY] === 0) {
+          nextY++;
         }
+        if (nextY < this.size && this.board[nextX][nextY] === currentTileValue) {
+          canMerge = true;
+        }
+        if (j !== nextY - 1) {
+          moved = true;
+        }
+        this.board[i][j] = 0;
+        this.board[nextX][nextY - (canMerge ? 0 : 1)] = canMerge ? currentTileValue * 2 : currentTileValue;
+        }
+      }
       }
       break;
 
     case 'up':
       for (var j = 0; j < this.size; j++) {
-        for (var i = 1; i < this.size; i++) {
-          if (this.board[i][j] !== 0) {
-            currentTileValue = this.board[i][j];
-            nextX = i - 1;
-            nextY = j;
-            canMerge = false;
-            while (nextX >= 0 && this.board[nextX][nextY] === 0) {
-              nextX--;
-            }
-            if (nextX >= 0 && this.board[nextX][nextY] === currentTileValue) {
-              canMerge = true;
-            }
-            if (i !== nextX + 1) {
-              moved = true;
-            }
-            this.board[i][j] = 0;
-            this.board[nextX + (canMerge ? 0 : 1)][nextY] = canMerge ? currentTileValue * 2 : currentTileValue;
-          }
+      for (var i = 1; i < this.size; i++) {
+        if (this.board[i][j] !== 0) {
+        currentTileValue = this.board[i][j];
+        nextX = i - 1;
+        nextY = j;
+        canMerge = false;
+        while (nextX >= 0 && this.board[nextX][nextY] === 0) {
+          nextX--;
         }
+        if (nextX >= 0 && this.board[nextX][nextY] === currentTileValue) {
+          canMerge = true;
+        }
+        if (i !== nextX + 1) {
+          moved = true;
+        }
+        this.board[i][j] = 0;
+        this.board[nextX + (canMerge ? 0 : 1)][nextY] = canMerge ? currentTileValue * 2 : currentTileValue;
+        }
+      }
       }
       break;
 
     case 'down':
-  for (var j = 0; j < this.size; j++) {
-    for (var i = this.size - 2; i >= 0; i--) {
-      if (this.board[i][j] !== 0) {
+      for (var j = 0; j < this.size; j++) {
+      for (var i = this.size - 2; i >= 0; i--) {
+        if (this.board[i][j] !== 0) {
         currentTileValue = this.board[i][j];
         nextX = i + 1;
         nextY = j;
@@ -186,11 +186,30 @@ move: function(direction) {
         }
         this.board[i][j] = 0;
         this.board[nextX - (canMerge ? 0 : 1)][nextY] = canMerge ? currentTileValue * 2 : currentTileValue;
+        }
       }
+      }
+      break;
     }
+    this.updateBoard();
+  },
+  
+  bindEventListeners: function() {
+    var self = this;
+    document.addEventListener('keydown', function(event) {
+    if (event.keyCode === 37 || event.keyCode === 65) { // left arrow key or 'a' key
+      self.move('left');
+    }
+    if (event.keyCode === 38 || event.keyCode === 87) { // up arrow key or 'w' key
+      self.move('up');
+    }
+    if (event.keyCode === 39 || event.keyCode === 68) { // right arrow key or 'd' key
+      self.move('right');
+    }
+    if (event.keyCode === 40 || event.keyCode === 83) { // down arrow key or 's' key
+      self.move('down');
+    }
+    });
   }
-  break;
 }
-}
-};
 GameManager.setup();
