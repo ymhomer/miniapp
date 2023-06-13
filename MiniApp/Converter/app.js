d53d5d74c78c8b967865f2b039892aba371ce039
ymhomer <ymhomer.4253@gmail.com>
2023-06-13 09:05:08 +0800

    Update app.js

--- MiniApp/Converter/app.js
+++ MiniApp/Converter/app.js
@@ -1,61 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById("addQuote").addEventListener('click', convert);
-
-    function convert() {
-        // get textarea
-        let textareaContent = document.getElementById("ta_text").value;
-
-        // split content line by line
-        let items = textareaContent.split("\n");
-
-        // 去除每个元素的前后空格
-        items = items.map(item => item.trim());
-
-        // If Uppercase
-        if (document.getElementById("uppercaseCheck").checked) {
-            items = items.map(item => item.toUpperCase());
-        }
-
-        // convert
-        let result = "('" + items.join("', '") + "')";
-
-        // update the result to textarea
-        document.getElementById("ta_text").value = result;
-    }
-
-    document.getElementById("removeQuote").addEventListener('click', revert);
-
-    function revert() {
-        // get textarea
-        let textareaContent = document.getElementById("ta_text").value;
-
-        // remove Brackets
-        let removedBrackets = textareaContent.slice(2, -2);
-
-        // split value
-        let items = removedBrackets.split("', '");
-
-        // If Uppercase
-        if (document.getElementById("uppercaseCheck").checked) {
-            items = items.map(item => item.toUpperCase());
-        }
-
-        // convert
-        let result = items.join("\n");
-
-        // update the result to textarea
-        document.getElementById("ta_text").value = result;
-    }
-
-    //Pendding feature
     let inputElement = document.getElementById('ta_text');
-
-    inputElement.addEventListener('paste', function(event) {
-        let pasteData = event.clipboardData.getData('text');
-        console.log(pasteData);
-
-        // Action
-    });
+    document.getElementById("addQuote").addEventListener('click', convert);
+    document.getElementById("removeQuote").addEventListener('click', revert);
 
     document.getElementById("copyText").addEventListener('click', async () => {
         let textareaContent = document.getElementById("ta_text").value;
@@ -82,4 +28,74 @@ document.addEventListener('DOMContentLoaded', function() {
             console.error('Failed to copy text: ', err);
         }
     });
+
+    //Pendding feature - get textarea status
+    document.getElementById('ta_text').addEventListener('paste', function(event) {
+        setTimeout(function() {
+            
+            let status = checkTextareaStatus();
+            console.log("Status after paste: " + status);
+        }, 0);
+    });
+
+    function convert() {
+        // get textarea
+        let textareaContent = document.getElementById("ta_text").value;
+
+        // split content line by line
+        let items = textareaContent.split("\n");
+
+        // 去除每个元素的前后空格
+        items = items.map(item => item.trim());
+
+        // If Uppercase
+        if (document.getElementById("uppercaseCheck").checked) {
+            items = items.map(item => item.toUpperCase());
+        }
+
+        // convert
+        let result = "('" + items.join("', '") + "')";
+
+        // update the result to textarea
+        document.getElementById("ta_text").value = result;
+    }
+
+    function revert() {
+        // get textarea
+        let textareaContent = document.getElementById("ta_text").value;
+
+        // remove Brackets
+        let removedBrackets = textareaContent.slice(2, -2);
+
+        // split value
+        let items = removedBrackets.split("', '");
+
+        // If Uppercase
+        if (document.getElementById("uppercaseCheck").checked) {
+            items = items.map(item => item.toUpperCase());
+        }
+
+        // convert
+        let result = items.join("\n");
+
+        // update the result to textarea
+        document.getElementById("ta_text").value = result;
+    }
+
+    function checkTextareaStatus() {
+        let textareaContent = document.getElementById("ta_text").value.trim();
+
+        // Check select
+        if (textareaContent.toUpperCase().startsWith("SELECT")) {
+            return "select_statement";
+        }
+        // Check quote
+        else if (textareaContent.startsWith("('") && textareaContent.endsWith("')")) {
+            return "converted";
+        }
+        // Else "Default"
+        else {
+            return "default";
+        }
+    }
 });
