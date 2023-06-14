e3e6c028eff00850dc45fbccf8780599dc09d3e2
ymhomer <ymhomer.4253@gmail.com>
2023-06-15 06:13:20 +0800

    Update app.js

--- About/app.js
+++ About/app.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
   fetch('/miniapp/README.md')
     .then(response => {
-      console.log('123');
+      console.log("123");
         console.log(response.url); // 输出你的 fetch 请求的 URL
         return response.text();
     })
