1585ff5216a883d1edb271d7c5da4f23ae7a6ea9
ymhomer <ymhomer.4253@gmail.com>
2023-08-10 01:40:45 +0800

    Update app.js

--- MiniApp/Scoreboard/app.js
+++ MiniApp/Scoreboard/app.js
@@ -2,3 +2,11 @@ document.getElementById('scoreRed').addEventListener('click', function() {
     let currentScore = parseInt(this.textContent, 10);
     this.textContent = currentScore + 1;
 });
+
+document.querySelectorAll('.dropdown-item').forEach(item => {
+  item.addEventListener('click', function() {
+    if (!this.classList.contains('disabled')) {
+      document.getElementById('dropdownMenuButton').textContent = this.textContent;
+    }
+  });
+});
