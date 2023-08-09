7897a2323cb67a2cf60da3d7c408c423d4abc8e3
ymhomer <ymhomer.4253@gmail.com>
2023-08-10 06:39:42 +0800

    Scoreboard mark logic update

--- MiniApp/Scoreboard/app.js
+++ MiniApp/Scoreboard/app.js
@@ -1,20 +1,32 @@
 document.addEventListener("DOMContentLoaded", function() {
     const teamRed = document.getElementById('teamRed');
     const teamBlue = document.getElementById('teamBlue');
+    const redD = document.getElementById('red-1');
+    const blueD = document.getElementById('blue-1');
     const scoreRed = document.getElementById('scoreRed');
     const scoreBlue = document.getElementById('scoreBlue');
     const singleRoundScoreSlt = document.getElementById('singleRoundScoreSlt');
     const singleRoundScoreCustom = document.getElementById('singleRoundScoreCustom');
     var singleRoundScore = singleRoundScoreSlt.value;
 
-    document.getElementById('teamRed').addEventListener('click', function() {
+    teamRed.addEventListener('click', function() {
         let currentScore = parseInt(scoreRed.textContent, 10);
         scoreRed.textContent = currentScore + 1;
     });
 
-    document.getElementById('teamBlue').addEventListener('click', function() {
+    teamBlue.addEventListener('click', function() {
         let currentScore = parseInt(scoreBlue.textContent, 10);
         scoreBlue.textContent = currentScore + 1;
+    });
+
+    redD.addEventListener('click', function() {
+        let currentScore = parseInt(scoreRed.textContent, 10);
+        scoreRed.textContent = currentScore - 1;
+    });
+
+    blueD.addEventListener('click', function() {
+        let currentScore = parseInt(scoreBlue.textContent, 10);
+        scoreBlue.textContent = currentScore - 1;
     });
 
     singleRoundScoreSlt.addEventListener('change', function() {
