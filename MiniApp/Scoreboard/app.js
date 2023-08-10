4ddcaee478249d1534fc140564b8294cee3ad604
ymhomer <ymhomer.4253@gmail.com>
2023-08-11 03:08:53 +0800

    Update app.js

--- MiniApp/Scoreboard/app.js
+++ MiniApp/Scoreboard/app.js
@@ -3,6 +3,9 @@ document.addEventListener("DOMContentLoaded", function() {
     const teamBlue = document.getElementById('teamBlue');
     const redD = document.getElementById('red-1');
     const blueD = document.getElementById('blue-1');
+    const newGameBtn = document.getElementById('newGameBtn');
+    const confirmEndGame = document.getElementById('confirmEndGame');
+    const cancelEndGame = document.getElementById('cancelEndGame');
     const scoreRed = document.getElementById('scoreRed');
     const scoreBlue = document.getElementById('scoreBlue');
     const singleRoundScoreSlt = document.getElementById('singleRoundScoreSlt');
@@ -41,44 +44,19 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 
-    document.getElementById('newGameBtn').addEventListener('click', function() {
+    newGameBtn.addEventListener('click', function() {
         scoreRed.textContent = '0';
         scoreBlue.textContent = '0';
     });
 
     function checkScore(team) {
-        //let maxScore = singleRoundScore !== '0' ? parseInt(singleRoundScore, 10) : parseInt(singleRoundScoreCustom.value, 10);
-        //let currentScore = team === 'red' ? parseInt(scoreRed.textContent, 10) : parseInt(scoreBlue.textContent, 10);
-        
         if (parseInt(scoreRed.textContent, 10) >= maxScore || parseInt(scoreBlue.textContent, 10) >= maxScore) {
             var confirmModal = new bootstrap.Modal(document.getElementById('confirmModal'));
             confirmModal.show();
         }
-/*
-
-        if (currentScore >= maxScore) {
-            //let isConfirmed = confirm('Are you sure about this score?');
-            var modal = new bootstrap.Modal(document.getElementById('confirmModal'));
-            modal.show();
-            if (isConfirmed) {
-                alert(`${team === 'red' ? 'Red' : 'Blue'} team wins!`);
-                // TODO: Add the result to the history.
-            } else {
-                // Subtract the last added score.
-                if (team === 'red') {
-                    scoreRed.textContent = currentScore - 1;
-                } else {
-                    scoreBlue.textContent = currentScore - 1;
-                }
-            }
-        }*/
     }
-    /*
-    document.getElementById('confirmEndGame').addEventListener('click', function() {
-        // 在这里处理用户确认结束游戏的逻辑
-        modal.hide();
-    });*/
-    document.getElementById('confirmEndGame').addEventListener('click', function() {
+
+    confirmEndGame.addEventListener('click', function() {
         // 根据之前的逻辑确定哪个队伍胜利
         if (parseInt(scoreRed.textContent, 10) >= maxScore) {
             document.getElementById('winningTeamName').textContent = "Red Team";
@@ -86,27 +64,20 @@ document.addEventListener("DOMContentLoaded", function() {
             document.getElementById('winningTeamName').textContent = "Blue Team";
         }
 
-        // 显示胜利模态框
         var victoryModal = new bootstrap.Modal(document.getElementById('victoryModal'));
         victoryModal.show();
 
-        // 关闭确认模态框
         var confirmModal = bootstrap.Modal.getInstance(document.getElementById('confirmModal'));
         confirmModal.hide();
     });
 
-    document.getElementById('cancelEndGame').addEventListener('click', function() {
-        // 扣除最后添加的分数
+    cancelEndGame.addEventListener('click', function() {
         if (parseInt(scoreRed.textContent, 10) >= maxScore) {
             scoreRed.textContent = parseInt(scoreRed.textContent,10)-1;
         } else if (parseInt(scoreBlue.textContent, 10) >= maxScore) {
             scoreBlue.textContent = parseInt(scoreBlue.textContent,10)-1;
         }
 
-        // 更新分数显示
-        //updateScoreDisplay();
-
-        // 关闭确认模态框
         var confirmModal = bootstrap.Modal.getInstance(document.getElementById('confirmModal'));
         confirmModal.hide();
     });
