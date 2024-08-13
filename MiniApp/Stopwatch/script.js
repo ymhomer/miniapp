632d192b8189a39d722e85fa3fc5aee6b3a4c610
ymhomer <ymhomer.4253@gmail.com>
2024-08-13 20:00:25 +0800

    Stopwatch - update UI and logic

--- MiniApp/Stopwatch/script.js
+++ MiniApp/Stopwatch/script.js
@@ -3,7 +3,11 @@ let stopwatchRunning = false;
 let stopwatchPaused = false;
 let stopwatchTime = 0;
 let stopwatchInterval;
-//let wakeLock = null;
+let wakeLock = null;
+
+
+const keepAwakeCheckbox = document.getElementById('keep-awake');
+const fullscreenCheckbox = document.getElementById('fullscreen');
 
 function updateTime() {
     const now = new Date();
@@ -77,8 +81,8 @@ function updateStopwatchDisplay() {
 }
 
 function initializeSettings() {
-    const keepAwakeCheckbox = document.getElementById('keep-awake');
     keepAwakeCheckbox.checked = true;
+    requestWakeLock();
     
     if ('wakeLock' in navigator) {
         navigator.wakeLock.request('screen');
@@ -86,17 +90,13 @@ function initializeSettings() {
 
     keepAwakeCheckbox.addEventListener('change', function () {
         if (this.checked) {
-            if ('wakeLock' in navigator) {
-                navigator.wakeLock.request('screen');
-            }
+            requestWakeLock();
         } else {
-            if ('wakeLock' in navigator) {
-                navigator.wakeLock.release();
-            }
+            releaseWakeLock();
         }
     });
 
-    const fullscreenCheckbox = document.getElementById('fullscreen');
+    
     fullscreenCheckbox.addEventListener('change', function () {
         if (this.checked) {
             enterFullscreen();
@@ -104,7 +104,7 @@ function initializeSettings() {
             exitFullscreen();
         }
     });
-
+    /*
     function enterFullscreen() {
         const elem = document.documentElement;
         if (elem.requestFullscreen) {
@@ -138,17 +138,107 @@ function initializeSettings() {
                 document.documentElement.requestFullscreen();
             }
         }
-    });
+    });*/
+    document.addEventListener('fullscreenchange', handleFullscreenChange);
+    document.addEventListener('webkitfullscreenchange', handleFullscreenChange);
+    document.addEventListener('mozfullscreenchange', handleFullscreenChange);
+    document.addEventListener('msfullscreenchange', handleFullscreenChange);
 }
 
 function handleFullscreenChange() {
-    const fullscreenCheckbox = document.getElementById('fullscreen');
     const isFullscreen = !!document.fullscreenElement || 
                          !!document.mozFullScreenElement || 
                          !!document.webkitFullscreenElement || 
                          !!document.msFullscreenElement;
                          
     fullscreenCheckbox.checked = isFullscreen;
+}
+
+async function requestWakeLock() {
+    try {
+        if ('wakeLock' in navigator && wakeLock === null) {
+            wakeLock = await navigator.wakeLock.request('screen');
+            wakeLock.addEventListener('release', () => {
+                console.log('Wake Lock was released');
+                updateKeepAwakeCheckbox(false);
+            });
+            console.log('Wake Lock is active');
+        }
+    } catch (err) {
+        console.error(`${err.name}, ${err.message}`);
+    }
+}
+
+function releaseWakeLock() {
+    if (wakeLock !== null) {
+        wakeLock.release().then(() => {
+            wakeLock = null;
+            console.log('Wake Lock released manually');
+        });
+    }
+}
+
+function updateKeepAwakeCheckbox(isLocked) {
+    const keepAwakeCheckbox = document.getElementById('keep-awake');
+    keepAwakeCheckbox.checked = isLocked;
+}
+
+function updateKeepAwakeCheckbox(isLocked) {
+    const keepAwakeCheckbox = document.getElementById('keep-awake');
+    keepAwakeCheckbox.checked = isLocked;
+}
+
+function enterFullscreen() {
+    const elem = document.documentElement;
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.mozRequestFullScreen) { // Firefox
+        elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) { // Chrome, Safari and Opera
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { // IE/Edge
+        elem.msRequestFullscreen();
+    }
+}
+
+function exitFullscreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) { // Firefox
+        document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) { // Chrome, Safari and Opera
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { // IE/Edge
+        document.msExitFullscreen();
+    }
+}function updateKeepAwakeCheckbox(isLocked) {
+    const keepAwakeCheckbox = document.getElementById('keep-awake');
+    keepAwakeCheckbox.checked = isLocked;
+}
+
+function enterFullscreen() {
+    const elem = document.documentElement;
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.mozRequestFullScreen) { // Firefox
+        elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) { // Chrome, Safari and Opera
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { // IE/Edge
+        elem.msRequestFullscreen();
+    }
+}
+
+function exitFullscreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) { // Firefox
+        document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) { // Chrome, Safari and Opera
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { // IE/Edge
+        document.msExitFullscreen();
+    }
 }
 
 setInterval(updateTime, 1000);
